00e82a7c81bf9a809e0f95b18f7fdba84308cd5a
garybot <argot.pryer@gmail.com>
2020-08-16 19:52:07 -0700

    self.move_right() complete robot_sort with a solution that meets stretch.

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -93,12 +93,26 @@ class SortingRobot:
         return self._light == "ON"
 
     def sort(self):
-        """
-        Sort the robot's list.
-        """
-        # Fill this out
-        pass
-
+        self.set_light_on()
+        self.swap_item()
+        while self.light_is_on():
+            self.set_light_off()
+            while self.can_move_right():
+                self.move_right()
+                if self.compare_item() is None:
+                    self.swap_item()
+                elif self.compare_item() is not None and self.compare_item() > 0:
+                    self.swap_item()
+                    self.set_light_on()
+            while self.can_move_left():
+                self.move_left()
+                if self.compare_item() is None:
+                    self.swap_item()
+                    break
+        # Super hacky :/
+        while self.can_move_right():
+            self.move_right()
+        self.swap_item()
 
 if __name__ == "__main__":
     # Test our your implementation from the command line
